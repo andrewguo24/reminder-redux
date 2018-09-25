390c3f55dbf237280f41c35b07631ff22fdee7c4
Ge Guo <alex@Ges-MacBook-Pro.local>
2018-09-26 00:11:05 +1000

    Fifth Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
@@ -13,7 +12,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 const store = createStore(
     rootReducer,
     composeWithDevTools(
-    applyMiddleware(logger)
+        applyMiddleware(logger)
     )
 );
 
